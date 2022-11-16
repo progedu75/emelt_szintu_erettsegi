6f858ea02b579b8e1fb00861fa9a84b39d9f5338
ProgEdu <progedu75@gmail.com>
2022-11-16 20:53:30 +0100

    init

--- 2015_oktober_emelt/fejvagyiras.py
+++ 2015_oktober_emelt/fejvagyiras.py
@@ -1,4 +1,14 @@
 import random
 lehetoseg = ['I', 'F']
-print('\1. feladat:')
+print('\n1. feladat:')
 print(f'A pénzfeldobás eredménye: {random.choice(lehetoseg)}')
+
+print('\n2. feladat:')
+usertipp = input('Tippeljen! (F/I): ')
+tipp = random.choice(lehetoseg)
+
+print(f'A tipp {usertipp.upper()}, a dobás eredménye {tipp} volt.')
+if usertipp == tipp:
+    print('Ön eltalálta!')
+else:
+    print('Ön nem találta el!')
